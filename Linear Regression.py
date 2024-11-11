a2d9af0e736546252720a40e38be757edadef436
Hyeongyo-Song <songhg0321@naver.com>
2024-11-11 22:39:31 +0900

    Add files via upload

---
+++ Linear Regression.py
@@ -0,0 +1,20 @@
+import torch
+
+x_train = torch.FloatTensor([[0],[1],[2]])
+y_train = torch.FloatTensor([[0],[1],[2]])
+
+weight = torch.zeros(1, requires_grad=True)
+bias = torch.zeros(1, requires_grad=True)
+
+optimizer = torch.optim.SGD([weight,bias], lr = 0.01)
+
+nb_epochs = 1000
+for epoch in range(1, nb_epochs+1):
+    hypothesis = weight * x_train + bias
+    cost = torch.mean((hypothesis - y_train) ** 2)
+
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+print(weight * 3 + bias)
