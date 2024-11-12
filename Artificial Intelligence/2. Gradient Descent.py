744d9695a63b93576c4fd54b62b47632f9d18db5
Hyeongyo-Song <songhg0321@naver.com>
2024-11-12 15:14:28 +0900

    Update 2. Gradient Descent.py

--- Artificial Intelligence/2. Gradient Descent.py
+++ Artificial Intelligence/2. Gradient Descent.py
@@ -5,15 +5,15 @@ import torch
 x_train = torch.FloatTensor([[0],[1],[2]])
 y_train = torch.FloatTensor([[0],[1],[2]])
 
-weight = torch.zeros(1, True)
-bias = torch.zeros(1, True)
+weight = torch.zeros(1, True) # 두번째 Parameter는 'requires_grad = True'가 되어야 합니다. 본 문서에서는 오류가 없었지만 추후에 문제가 발생함을 확인하였음.
+bias = torch.zeros(1, True) # 마찬가지입니다.
 
-epochs = 10
+epochs = 10 # 총 10번 반복할 예정
 for epoch in range(epochs):
-    Hypothesis = weight * x_train + bias
-    loss = torch.mean((Hypothesis - y_train) ** 2)
-    Gradient = 2 * torch.mean((Hypothesis - y_train) * x_train)
-    lr = 0.1
-    weight -= lr * Gradient
+    Hypothesis = weight * x_train + bias # 정답 값 Y = Wx + b (W는 Weight, b는 bias를 의미합니다.)
+    loss = torch.mean((Hypothesis - y_train) ** 2) # 이전 문서와 동일하게 Mean Squared Error를 사용합니다. 잔차제곱평균이라고도 합니다.
+    Gradient = 2 * torch.mean((Hypothesis - y_train) * x_train) # 이전 문서에서는 optim.SGD를 통해 편하게 확률적 경사 하강법을 적용했지만, 본 문서에서는 직접 구현합니다. Gradient는 MSE를 Weight로 편미분하면 얻을 수 있습니다.
+    lr = 0.1 # 학습률(Learning Rate)입니다.
+    weight -= lr * Gradient # Gradient가 음수라면 우측으로, 양수라면 좌측으로 움직입니다. Learning Rate를 곱해줌으로써 이를 가능케 합니다.
 
     print('Epoch {}/{} W:{}, B:{}, Loss:{}'.format(epoch, epochs, weight, bias, loss))
