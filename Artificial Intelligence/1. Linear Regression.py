a2c81fa41cd56ff3e5843ea4be085b86c4151960
Hyeongyo-Song <songhg0321@naver.com>
2024-11-12 18:00:32 +0900

    Update 1. Linear Regression.py

--- Artificial Intelligence/1. Linear Regression.py
+++ Artificial Intelligence/1. Linear Regression.py
@@ -15,8 +15,8 @@ for epoch in range(1, nb_epochs+1):
     hypothesis = weight * x_train + bias # 예측값 Y Predict를 Hypothesis라 정의, 이는 Wx + b로 도출할 수 있습니다.
     cost = torch.mean((hypothesis - y_train) ** 2) # Mean Squared Error(MSE)를 Loss Function으로 사용합니다. 본 문서에서는 직접 구현.
 
-    optimizer.zero_grad() # 세트로 따라옴 !
-    cost.backward() # 세트로 따라옴 !
-    optimizer.step() # 세트로 따라옴 !
+    optimizer.zero_grad() # 이전에 구한 Gradient가 있으면, 0으로 초기화함. 초기화하지 않으면 기존의 Gradient에 계속 더해짐.
+    cost.backward() # Loss를 BackPropagation 합니다.
+    optimizer.step() # Loss를 최소화하는 방향으로 학습을 진행 !
 
 print(weight * 3 + bias) # 3이라는 실제 값을 주었을때 Predict 값은 ?
