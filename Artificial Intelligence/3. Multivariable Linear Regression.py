97d26973f46ab58ebae4cff121723935428b0753
Hyeongyo-Song <songhg0321@naver.com>
2024-11-12 23:17:30 +0900

    Update 3. Multivariable Linear Regression.py

--- Artificial Intelligence/3. Multivariable Linear Regression.py
+++ Artificial Intelligence/3. Multivariable Linear Regression.py
@@ -1,4 +1,15 @@
-# 3. Multivariate Linear Regression을 구현하시오.
+# Multivariate Linear Regression을 구현하시오.
+
+이전에 구현해본 Linear Regression은 단순한 Y = Wx+b 형태의 구조였습니다.
+이를 Simple Linear Regression이라 합니다.
+
+그렇다면 x와 W가 여러 개일 때는 어떻게 해야 할까요 ?
+
+본 문서에서는 Input Tensor와 Output Tensor의 크기를 맞춰주고, matmul Method를 사용하여 다차원의 데이터를 쉽게 처리할 수 있었습니다. 함께 보실까요 ?
+
+- 송현교 작성
+
+-------------------------------------------------------------------------------------------------------------------------------
 
 import torch
 
@@ -16,7 +27,7 @@ optimizer = torch.optim.SGD([weight, bias], 1e-5) # Optimizer는 언제나처럼
 
 epochs = 1000 # 총 1000회 반복합니다.
 for epoch in range(epochs):
-    Hypothesis = x_train.matmul(weight) + bias # Y = Wx + b를 코드로 표현합니다.
+    Hypothesis = x_train.matmul(weight) + bias # Y = W1x1 + W2x2 + W3x3 + ... + Wnxn + b를 코드로 표현합니다.
     cost = torch.mean((Hypothesis - y_train) ** 2) # Mean Squared Error 사용.
 
     optimizer.zero_grad() # 언제나 세트로 와야 합니다.
